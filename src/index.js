cc19c34099f4e681d1a1100d9d53c9089dc5e6ac
Oleksandr Lutsiuk <oleksandr.lutsiuk@gmail.com>
2023-06-14 20:18:02 +0300

    add redux thunk & devtools

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./App.css";
-import { createStore } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import { rootReducer } from "./redux/rootReducer";
 import { Provider } from "react-redux";
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
